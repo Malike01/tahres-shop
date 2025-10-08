6b0779ae62ee0112634913d53f56b81dd6ed6618
Malike Sen <malikesen61@gmail.com>
2025-10-08 16:20:06 +0300

    feat(redux): Add Redux Toolkit for global state management
    Integrates Redux Toolkit to manage the application's global state, establishing a scalable and maintainable architecture.
    
    - Sets up the main Redux store using `configureStore`.
    - Creates typed `useAppDispatch` and `useAppSelector` hooks for improved TypeScript safety.
    - Wraps the entire application in the Redux `<Provider>` to make the store available to all components.

--- src/main.tsx
+++ src/main.tsx
@@ -6,18 +6,21 @@ import { AuthProvider } from './auth/AuthProvider.tsx'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import './i18n';
 import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux';
+import { store } from './store/index.ts'
 
 const queryClient = new QueryClient();
 
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <AuthProvider>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </AuthProvider>
-    </QueryClientProvider>
+    <Provider store={store}>
+      <QueryClientProvider client={queryClient}>
+        <AuthProvider>
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </AuthProvider>
+      </QueryClientProvider>
+    </Provider>
   </StrictMode>,
 )
